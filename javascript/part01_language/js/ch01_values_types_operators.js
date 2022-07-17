a2679bc9877e83bfd7fc19434ce1d22ca897f56e
malkzhub <da.malcolmj@gmail.com>
2022-07-17 21:39:55 +0800

    Ended at page 61

---
+++ javascript/part01_language/js/ch01_values_types_operators.js
@@ -0,0 +1,137 @@
+// // #### 2022-07-16d
+// //// Values
+// let x = 'whoosh';
+// console.log('the value of x is ' + x);
+
+// //// Numbers
+// let fraction = 9.81;
+// console.log(fraction);
+
+// let exponent = 2998e8;
+// console.log(exponent);
+
+// //// Arithmetic
+// // Addition
+// console.log(2 + 3) // 5
+
+// // Subtraction
+// console.log(2 - 3) // -1
+
+// // Multiplication
+// console.log(2 * 3) // 6
+
+// // Division
+// console.log(3 / 2) // 1.5
+
+// // Modulus
+// console.log(3 % 2) // 1
+
+// // Exponentiation
+// console.log(3 ** 3) // 27
+
+// // Other Samples
+// console.log(100 + 4 * 11) // 144
+// console.log((100 + 4) * 11) // 1144
+
+// // Special Numbers
+// console.log(Infinity);
+// console.log(-Infinity);
+// console.log(Infinity - 1); // Infinity
+// console.log(0 / 0) // NaN - not a number
+// console.log(Infinity - Infinity) // NaN
+
+// //// Strings
+// // Basic
+// console.log('Down on the sea');
+// console.log("Lie on the ocean");
+// console.log('Float on the ocean');
+
+// // Newlines
+// console.log("This is the first line\nAnd this is the second.");
+// // This is the first line
+// // And this is the second line
+
+
+// console.log("A newline character is written like \"\n\".");
+// // A newline character is written like "
+// // ".
+
+// // Concatenate
+// console.log("con" + "cat" + "e" + "nate");
+// // concatenate
+
+// // template literals
+// console.log(`half of 100 is ${100 / 2}`);
+// // half of 100 is 50
+
+// // ####################################################################
+
+// // #### 2022-07-17 
+
+// //// Unary Operators
+// console.log(typeof 4.5); // -> number
+// console.log(typeof "x"); // -> string
+// console.log(- (10 - 2)); // -8
+
+// //// Boolean Values
+
+// // Comparison
+// console.log(3 > 2); // true
+// console.log(3 < 2); // false
+// console.log("Aardvark" < "Zoroaster"); // true
+// console.log("Itchy" != "Scratchy"); // true
+// console.log("Apple" == "Orange"); // false
+// console.log(NaN == NaN); // false; NaN is not equal to itself
+
+// console.log();
+
+// // Logical Operators
+// console.log(true && false); // false
+// console.log(true && true); // true
+// console.log(false || true); // true
+// console.log(false || false); // false
+// console.log(!true); // false
+// console.log(!false); // true
+
+// console.log();
+
+// console.log((1 + 1 == 2) && (10 * 10 > 50)); // true
+
+// console.log();
+
+// // Ternary or Conditional operator
+// console.log(true ? 1 : 2 ); // 1
+// console.log(false ? 1 : 2); // 2
+
+// console.log();
+
+// // Empty Values
+// console.log(null);
+// console.log(undefined);
+
+// console.log();
+
+// //// Automatic Type Conversion
+// console.log(8 * null); // 0
+// console.log("5" - 1); // 4
+// console.log("5" + 1); // 51
+// console.log("five" * 2); // NaN
+// console.log(false == 0); // true
+
+
+// console.log();
+
+// console.log(null == undefined); // true
+// console.log(null == 0); // false
+
+// console.log();
+
+// console.log("" === false); // false
+// console.log("" !== false); // true
+
+
+// //// Short-circuiting of Logical Operators
+// console.log();
+
+// console.log(null || "user"); // user
+// console.log("Agnes" || "user"); // Agnes
---
+++ javascript/part01_language/js/ch02_program_structure.js
@@ -0,0 +1,39 @@
+// // #### 2022-07-17
+
+//// Binding or Variable
+let caught = 5 * 5;
+console.log(caught); // 25
+
+let ten = 10;
+console.log(ten * ten); // 100
+
+let mood = "light";
+console.log(mood); // light
+
+mood = "dark";
+console.log(mood); // dark
+
+// Number of dollars that Luigi owes after paying $35
+let luigiDebt = 140;
+luigiDebt = luigiDebt - 35;
+console.log("$" + luigiDebt); // 105
+
+//
+let one = 1, two = 2;
+console.log(one + two); // 3
+
+// 
+var name = "Ayda";
+const greeting = "Hello ";
+console.log(greeting + name); // Hello Ayda
+
+// Javascript keywords and reserved words
+// ######
+// break, case, catch, class, const, continue, debugger, default,
+// delete, do, else, enum, export, extends, false, finally, for,
+// function, if, implements, import, interface, in, instanceof, let
+// new, package, private, protected, public, return, static, super,
+// switch, this, throw, true, try, typeof, var, void, while, with, yield
+
+// Functions
+// prompt("Enter Passcode:"); // view in browser
