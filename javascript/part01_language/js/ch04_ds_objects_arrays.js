742f2e6ba911bc43fa38ddd3479a7a50aca679fa
malkzhub <da.malcolmj@gmail.com>
2022-08-20 09:11:17 +0800

    Review Chapter 4 then proceed to Chapter 5

--- javascript/part01_language/js/ch04_ds_objects_arrays.js
+++ javascript/part01_language/js/ch04_ds_objects_arrays.js
@@ -674,3 +674,100 @@ let arrayValue = [1, 2, 3, 4, 5];
 reverseArrayInPlace(arrayValue);
 console.log(arrayValue);
 // [5, 4, 3, 2, 1]
+
+
+console.log();
+
+//// ###### A list
+
+// Objects, as generic blobs of values, can be used to build all sorts of data
+// structures. A common data structure is the list (not to be confused with
+// array). A list is a nested set of objects, with the first object holding a
+// reference to the second, the second to the third, and so on.
+
+function arrayToList(array) {
+    let list = null;
+    for(let i = array.length - 1; i >= 0; i--) {
+        list = {
+            value: array[i],
+            rest: list
+        };
+    }
+
+    return list;
+}
+
+function listToArray(list) {
+    let array = [];
+    for(let node = list; node; node = node.rest) {
+        array.push(node.value);
+    }
+
+    return array;
+}
+
+function prepend(value, list) {
+    return {
+        value,
+        rest: list
+    }
+}
+
+function nth(list, n) {
+    if(!list) return undefined;
+    else if(n == 0) return list.value;
+    else return nth(list.rest, n - 1);
+}
+
+console.log(arrayToList([10, 20]));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(listToArray(arrayToList([10, 20, 30])));
+// → [10, 20, 30]
+console.log(prepend(10, prepend(20, null)));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(nth(arrayToList([10, 20, 30]), 1));
+// → 20
+
+console.log();
+
+
+//// ###### Deep Comparison
+
+// The == operator compares objects by identity. But sometimes you’d
+// prefer to compare the values of their actual properties.
+// Write a function deepEqual that takes two values and returns true only
+// if they are the same value or are objects with the same properties, where
+// the values of the properties are equal when compared with a recursive
+// call to deepEqual.
+// To find out whether values should be compared directly (use the ===
+// operator for that) or have their properties compared, you can use the
+// typeof operator. If it produces "object" for both values, you should do a
+// deep comparison. But you have to take one silly exception into account:
+// because of a historical accident, typeof null also produces "object".
+// The Object.keys function will be useful when you need to go over the
+// properties of objects to compare them.
+
+function deepEqual(a, b) {
+    if(a === b) return true;
+
+    if(a == null || typeof a != "object" ||
+    b == null || typeof b != "object") return false;
+
+    let keysA = Object.keys(a), keysB = Object.keys(b);
+
+    if(keysA.length != keysB.length) return false;
+
+    for(let key of keysA) {
+        if(!keysB.includes(key) || !deepEqual(a[key], b[key])) return false;
+    }
+
+    return true;
+}
+
+let obj = {here: {is: "an"}, object: 2};
+console.log(deepEqual(obj, obj));
+// → true
+console.log(deepEqual(obj, {here: 1, object: 2}));
+// → false
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+// → true
