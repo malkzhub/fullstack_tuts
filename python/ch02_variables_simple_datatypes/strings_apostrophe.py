2a664fbb07cbf5eb38b479d0c3ad80b18ba90f6b
malkzhub <da.malcolmj@gmail.com>
2022-08-20 14:18:28 +0800

    Side by side python

---
+++ python/ch02_variables_simple_datatypes/strings_apostrophe.py
@@ -0,0 +1,9 @@
+#### Strings
+
+## Avoiding Syntax Errors with Strings
+
+message = "One of Python's strengths is its divers community"
+print(message)
+
+# if using all single quotes
+# message = 'One of Pythons's  stengths is its divers community' # error
---
+++ python/ch02_variables_simple_datatypes/strings_name.py
@@ -0,0 +1,10 @@
+##### Strings
+
+## CHanging Case in a String with Methods
+
+name = "ada lovelace"
+print(name.title()) # Ada Lovelace
+
+name = "Ada Lovelace"
+print(name.upper()) # ADA LOVELACE
+print(name.lower()) # ada lovelace
