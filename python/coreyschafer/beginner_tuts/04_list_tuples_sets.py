becc12af8ce263e5c6487b23bdef9c6c1f7c74de
malkzhub <da.malcolmj@gmail.com>
2022-11-22 23:01:40 +0800

    Proceed to 10:12 Corey Schafer Python Tutorial for Beginners 4: Lists, Tuples, and Sets

---
+++ python/coreyschafer/beginner_tuts/04_list_tuples_sets.py
@@ -0,0 +1,53 @@
+### Lists
+
+courses = ['History', 'Math', 'Physics', 'CompSci']
+
+print(len(courses)) # 4
+print(courses)
+# ['History', 'Math', 'Physics', 'CompSci']
+
+print()
+
+print(courses[0]) # History
+print(courses[3]) # CompSci
+print(courses[-1]) # Compsci
+# print(courses[4]) # IndexError
+
+print()
+
+print(courses[0:2]) # ['History', 'Math']
+print(courses[:2]) # ['History', 'Math']
+print(courses[2:]) # ['Physics', 'CompSci']
+
+print()
+
+# Append Method - append()
+# Insert Method - append()
+# Extend Method - extend()
+# Remove Method - remove()
+# Pop Method - pop() - filo
+
+
+courses_2 = ['Art', 'Education']
+
+# courses.append('Art')
+# courses.insert(0, 'Art')
+# courses.extend(courses_2)
+# courses.remove('Math')
+# courses.pop()
+
+# print(courses) 
+# ['History', 'Math', 'Physics', 'CompSci', 'Art']
+# ['Art', 'History', 'Math', 'Physics', 'CompSci', 'Art']
+# ['History', 'Math', 'Physics', 'CompSci', 'Art', 'Education']
+# ['History', 'Physics', 'CompSci']
+# ['History', 'Math', 'Physics']
+
+print()
+
+# Sorting Lists
+
+
+
+
+
