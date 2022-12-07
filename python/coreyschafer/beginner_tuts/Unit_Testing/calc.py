426079b363be003d559a918c5fbfcbd71d6ea396
malkzhub <da.malcolmj@gmail.com>
2022-12-07 21:23:38 +0800

    Continue Unit Testing at 16:00 Corey Schafer YT

--- python/coreyschafer/beginner_tuts/Unit_Testing/calc.py
+++ python/coreyschafer/beginner_tuts/Unit_Testing/calc.py
@@ -1,3 +1,23 @@
 
 def add(x, y):
-    pass
+    """Add Function"""
+    return x + y
+
+def subtract(x, y):
+    """Subtract Function"""
+    return x -y
+
+def multiply(x, y):
+    """Multiply Function"""
+    return x * y
+    # return x ** y # AssertionError
+
+def divide(x, y):
+    """Divide Function"""
+    if y == 0:
+        raise ValueError('Cannot be divided by zero!')
+
+    return x / y
+    # return x // y # AssertionError - Must update unit testing file
+
+
---
+++ python/coreyschafer/beginner_tuts/Unit_Testing/test_calc.py
@@ -0,0 +1,48 @@
+import unittest
+import calc
+
+
+class TestCalc(unittest.TestCase):
+
+    ## When testing it is recommended to use (test_) at the start of the method
+    def test_add(self):
+        # result = calc.add(10, 5)
+        # self.assertEqual(result, 15) # Correct
+        # self.assertEqual(result, 14) # AssertionError
+
+        ## Edge Case
+        self.assertEqual(calc.add(10, 5), 15)
+        self.assertEqual(calc.add(-1, 1), 0)
+        self.assertEqual(calc.add(-1, -1), -2)
+
+
+    def test_subtract(self):
+
+        self.assertEqual(calc.subtract(10, 5), 5)
+        self.assertEqual(calc.subtract(-1, 1), -2)
+        self.assertEqual(calc.subtract(-1, -1), 0)
+
+    def test_multiply(self):
+        
+        self.assertEqual(calc.multiply(10, 5), 50)
+        self.assertEqual(calc.multiply(-1, 1), -1)
+        self.assertEqual(calc.multiply(-1, -1), 1)
+
+    def test_divide(self):
+
+        self.assertEqual(calc.divide(10, 5), 2)
+        self.assertEqual(calc.divide(-1, 1), -1)
+        self.assertEqual(calc.divide(-1, -1), 1)
+        self.assertEqual(calc.divide(5, 2), 2.5)
+
+        ## Testing Dividing by zero raises the correct error (Start again at 16:00 Corey Schafer YT)
+
+        ## First Way
+        self.assertRaises(ValueError, calc.divide, 10, 0)
+
+
+## script in order not to run python -m unittest file.py in terminal
+if __name__ == '__main__':
+    unittest.main()
+
+## Unit testing will run normally in the terminal
