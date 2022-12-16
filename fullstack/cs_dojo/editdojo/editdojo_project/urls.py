7c7a15a2e76304a170b36113f836a68793dd9e12
malkzhub <da.malcolmj@gmail.com>
2022-12-16 19:07:21 +0800

    2022-12-16

--- fullstack/cs_dojo/editdojo/editdojo_project/urls.py
+++ fullstack/cs_dojo/editdojo/editdojo_project/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 
 ## Custom
 from hello.views import myView
-from todo.views import todoView
+from todo.views import todoView, addTodo, deleteTodo
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +26,6 @@ urlpatterns = [
     ## Custom
     path('sayHello/', myView),
     path('todo/', todoView),
+    path('addTodo/', addTodo),
+    path('deleteTodo/<int:todo_id>/', deleteTodo),
 ]
--- fullstack/cs_dojo/editdojo/todo/views.py
+++ fullstack/cs_dojo/editdojo/todo/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import TodoItem
 
 # def todoView(request):
@@ -13,3 +13,17 @@ def todoView(request):
     {
         'all_items': all_todo_items
     })
+
+def addTodo(request):
+    # create a new todo all_items
+    # save
+    # redirect the browser to '/todo/'
+    
+    new_item = TodoItem(content = request.POST['content'])
+    new_item.save()
+    return HttpResponseRedirect('/todo/')
+
+def deleteTodo(request, todo_id):
+    item_to_delete = TodoItem.objects.get(id=todo_id)
+    item_to_delete.delete()
+    return HttpResponseRedirect('/todo/')
